a4b9b8ae8779aa0c9b47845352bca3d0959eca5f
Stav Shamir <stav.shamir@ecitele.com>
2019-01-07 15:31:08 +0200

    Add KafkaController

---
+++ src/main/java/com/stavshamir/springaroo/KafkaController.java
@@ -0,0 +1,26 @@
+package com.stavshamir.springaroo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    private final KafkaEndpointsService endpointsService;
+
+    @Autowired
+    public KafkaController(KafkaEndpointsService endpointsService) {
+        this.endpointsService = endpointsService;
+    }
+
+    @GetMapping("/endpoints")
+    public Set<KafkaEndpoint> endpoints() {
+        return endpointsService.getEndpoints();
+    }
+
+}
