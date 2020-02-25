d5f15e1b164f289083a5003a24ee736139090b91
Stav Shamir <stav.shamir@ecitele.com>
2020-02-25 14:34:20 +0200

    Change print to logging

--- example/src/main/java/io/github/stavshamir/swagger4kafka/example/consumers/ExampleConsumer.java
+++ example/src/main/java/io/github/stavshamir/swagger4kafka/example/consumers/ExampleConsumer.java
@@ -1,15 +1,19 @@
 package io.github.stavshamir.swagger4kafka.example.consumers;
 
 import io.github.stavshamir.swagger4kafka.example.dtos.ExamplePayloadDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ExampleConsumer {
 
+    private static Logger logger = LoggerFactory.getLogger(ExampleConsumer.class);
+
     @KafkaListener(topics = "example-topic", containerFactory = "exampleKafkaListenerContainerFactory")
     public void receiveExamplePayload(ExamplePayloadDto payload) {
-        System.out.println(payload);
+        logger.info("Received new message in example-topic: {}", payload.toString());
     }
 
 }
