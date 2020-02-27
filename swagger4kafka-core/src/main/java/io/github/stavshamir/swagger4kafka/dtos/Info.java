412913a43e8b4536dcff54374ae4c71df4fadefd
Stav Shamir <stav.shamir@ecitele.com>
2020-02-27 11:55:05 +0200

    Add no args constructor to Info and KafkaEndpoint

--- swagger4kafka-core/src/main/java/io/github/stavshamir/swagger4kafka/dtos/Info.java
+++ swagger4kafka-core/src/main/java/io/github/stavshamir/swagger4kafka/dtos/Info.java
@@ -1,13 +1,17 @@
 package io.github.stavshamir.swagger4kafka.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Info {
 
-    private final String serviceName;
-    private final String bootstrapServers;
+    private String serviceName;
+    private String bootstrapServers;
 
 }
--- swagger4kafka-core/src/main/java/io/github/stavshamir/swagger4kafka/dtos/KafkaEndpoint.java
+++ swagger4kafka-core/src/main/java/io/github/stavshamir/swagger4kafka/dtos/KafkaEndpoint.java
@@ -1,17 +1,21 @@
 package io.github.stavshamir.swagger4kafka.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class KafkaEndpoint {
 
-    private final String topic;
-    private final String payloadClassName;
-    private final String payloadModelName;
-    private final Map<String, Object> payloadExample;
+    private String topic;
+    private String payloadClassName;
+    private String payloadModelName;
+    private Map<String, Object> payloadExample;
 
 }
