c795815d78567d9f7661bbef83d040c21ef56c8c
Stav Shamir <stav.shamir@ecitele.com>
2020-02-27 09:10:07 +0200

    Remove integration tests project

--- swagger4kafka-integration-tests/src/main/kotlin/io/github/stavshamir/swagger4kafka/integration/dtos/ExamplePayloadDto.kt
+++
@@ -1,7 +0,0 @@
-package io.github.stavshamir.swagger4kafka.integration.dtos
-
-data class ExamplePayloadDto(val aString: String, val aLong: Long, val anEnum: ExampleEnum) {
-    enum class ExampleEnum {
-        FOO1, FOO2, FOO3
-    }
-}
