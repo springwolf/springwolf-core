a70e8bd6b8a03cc0484acec8c87fb20dcefc6244
Stav Shamir <shamir.stav@gmail.com>
2020-06-14 15:37:33 +0300

    Use "implementation project" dependencies for core and ui projects
    Closes #22.

--- swagger4kafka-example/src/main/java/io/github/stavshamir/swagger4kafka/example/configuration/Swagger4KafkaConfiguration.java
+++ swagger4kafka-example/src/main/java/io/github/stavshamir/swagger4kafka/example/configuration/Swagger4KafkaConfiguration.java
@@ -1,8 +1,12 @@
 package io.github.stavshamir.swagger4kafka.example.configuration;
 
 import com.google.common.collect.ImmutableMap;
+import io.github.stavshamir.swagger4kafka.asyncapi.types.info.Info;
+import io.github.stavshamir.swagger4kafka.asyncapi.types.server.Server;
+import io.github.stavshamir.swagger4kafka.configuration.AsyncApiDocket;
 import io.github.stavshamir.swagger4kafka.configuration.Docket;
 import io.github.stavshamir.swagger4kafka.configuration.EnableSwagger4Kafka;
+import io.github.stavshamir.swagger4kafka.configuration.protocol.KafkaProtocolConfiguration;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,6 +41,24 @@ public class Swagger4KafkaConfiguration {
                 .put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
                 .put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class)
                 .put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false)
+                .build();
+    }
+
+    @Bean
+    public AsyncApiDocket asyncApiDocket() {
+        Info info = Info.builder()
+                .version("1.0.0")
+                .title("swagger4kafka example project")
+                .build();
+
+        KafkaProtocolConfiguration kafka = KafkaProtocolConfiguration.builder()
+                .basePackage("io.github.stavshamir.swagger4kafka.example.consumers")
+                .build();
+
+        return AsyncApiDocket.builder()
+                .info(info)
+                .server("kafka", Server.kafka().url(BOOTSTRAP_SERVERS).build())
+                .protocol(kafka)
                 .build();
     }
 
