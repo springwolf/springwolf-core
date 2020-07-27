57ed4d10cb88ee7af8a3e6946d537ac68bc722b3
Stav Shamir <shamir.stav@gmail.com>
2020-07-27 12:43:19 +0300

    Fix bug the prevented generation of composite classes examples

--- springwolf-core/src/main/java/io/github/stavshamir/springwolf/schemas/DefaultSchemasService.java
+++ springwolf-core/src/main/java/io/github/stavshamir/springwolf/schemas/DefaultSchemasService.java
@@ -1,12 +1,12 @@
 package io.github.stavshamir.springwolf.schemas;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.swagger.oas.inflector.examples.ExampleBuilder;
 import io.swagger.oas.inflector.examples.models.Example;
 import io.swagger.oas.inflector.processors.JsonNodeExampleSerializer;
-import io.swagger.util.Json;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.models.media.Schema;
 import lombok.extern.slf4j.Slf4j;
@@ -27,14 +27,18 @@ public class DefaultSchemasService implements SchemasService {
 
     public DefaultSchemasService() {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
         SimpleModule simpleModule = new SimpleModule().addSerializer(new JsonNodeExampleSerializer());
-        Json.mapper().registerModule(simpleModule);
+        objectMapper.registerModule(simpleModule);
     }
 
     @Override
     public Map<String, Schema> getDefinitions() {
-        definitions.forEach((k, schema) -> setExample(schema));
+        // The examples must first be set as JSON strings (the inflector does not work otherwise)
+        definitions.forEach(this::buildExampleAsString);
+
+        // Then they must be deserialized to map, or they will be serialized as reguler string and not json by the
+        // object mapper
+        definitions.forEach(this::deserializeExampleToMap);
         return definitions;
     }
 
@@ -48,14 +52,21 @@ public class DefaultSchemasService implements SchemasService {
         return type.getSimpleName();
     }
 
-    private void setExample(Schema schema) {
+    private void buildExampleAsString(String k, Schema schema) {
         log.debug("Setting example for {}", schema.getName());
 
         Example example = ExampleBuilder.fromSchema(schema, definitions);
-        String exampleAsJson = Json.pretty(example);
-
         try {
-            schema.setExample(objectMapper.readValue(exampleAsJson, Map.class));
+            String exampleAsJson = objectMapper.writeValueAsString(example);
+            schema.setExample(exampleAsJson);
+        } catch (JsonProcessingException e) {
+            log.error("Failed to write example value as a string");
+        }
+    }
+
+    private void deserializeExampleToMap(String k, Schema schema) {
+        try {
+            schema.setExample(objectMapper.readValue((String) schema.getExample(), Map.class));
         } catch (IOException e) {
             log.error("Failed to convert example object of {} to map", schema.getName());
         }
--- springwolf-example/src/main/java/io/github/stavshamir/springwolf/example/dtos/AnotherPayloadDto.java
+++ springwolf-example/src/main/java/io/github/stavshamir/springwolf/example/dtos/AnotherPayloadDto.java
@@ -3,6 +3,7 @@ package io.github.stavshamir.springwolf.example.dtos;
 public class AnotherPayloadDto {
 
     private String foo;
+    private ExamplePayloadDto example;
 
     public String getFoo() {
         return foo;
@@ -10,6 +11,22 @@ public class AnotherPayloadDto {
 
     public void setFoo(String foo) {
         this.foo = foo;
+    }
+
+    public ExamplePayloadDto getExample() {
+        return example;
+    }
+
+    public void setExample(ExamplePayloadDto example) {
+        this.example = example;
+    }
+
+    @Override
+    public String toString() {
+        return "AnotherPayloadDto{" +
+                "foo='" + foo + '\'' +
+                ", example=" + example +
+                '}';
     }
 
 }
