11c0cc7d364d1e7079f387f3217809a4a9c86752
Stav Shamir <shamir.stav@gmail.com>
2021-12-17 20:37:47 +0200

    Update springwolf-kafka version

--- springwolf-plugins/springwolf-kafka-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/SpringwolfKafkaController.java
+++ springwolf-plugins/springwolf-kafka-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/SpringwolfKafkaController.java
@@ -3,7 +3,9 @@ package io.github.stavshamir.springwolf.asyncapi;
 import io.github.stavshamir.springwolf.producer.SpringwolfKafkaProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -17,6 +19,11 @@ public class SpringwolfKafkaController {
 
     @PostMapping("/publish")
     public void publish(@RequestParam String topic, @RequestBody Map<String, Object> payload) {
+        if (!kafkaProducer.isEnabled()) {
+            log.debug("Kafka producer is not enabled - message will not be published");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Kafka producer is not enabled");
+        }
+
         log.info("Publishing to kafka topic {}: {}", topic, payload);
         kafkaProducer.send(topic, payload);
     }
