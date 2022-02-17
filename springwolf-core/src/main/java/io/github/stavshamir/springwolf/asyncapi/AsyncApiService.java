1a8f58743da3443903e98f7fc13db51e5ff8d1af
Vemuri Sai Aravind <50715301+SaiAravindVemuri@users.noreply.github.com>
2022-02-17 11:30:06 +0530

    Update AsyncApiService.java
    Added @service  annotation in order to resolve below error 
    
    nested exception is org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'asyncApiController' defined in URL [jar:file:/home/vcap/app/BOOT-INF/lib/springwolf-core-0.2.0.jar!/io/github/stavshamir/springwolf/asyncapi/AsyncApiController.class]: Unsatisfied dependency expressed through constructor parameter 0; nested exception is org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'defaultAsyncApiService' defined in URL [jar:file:/home/vcap/app/BOOT-INF/lib/springwolf-core-0.2.0.jar!/io/github/stavshamir/springwolf/asyncapi/DefaultAsyncApiService.class]: Unsatisfied dependency expressed through constructor parameter 1; nested exception is org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'defaultChannelsService': Invocation of init method failed; nested exception is java.lang.IllegalArgumentException: Only single parameter KafkaListener methods are supported

--- springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/AsyncApiService.java
+++ springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/AsyncApiService.java
@@ -1,7 +1,9 @@
 package io.github.stavshamir.springwolf.asyncapi;
 
 import io.github.stavshamir.springwolf.asyncapi.types.AsyncAPI;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AsyncApiService {
 
     AsyncAPI getAsyncAPI();
