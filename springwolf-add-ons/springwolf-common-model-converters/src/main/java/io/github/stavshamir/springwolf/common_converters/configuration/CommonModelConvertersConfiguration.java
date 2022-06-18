01fbfd4a7a6e2b3aea1d156f8053491396198768
Stav Shamir <shamir.stav@gmail.com>
2022-06-18 21:52:01 +0300

    Release kafka plugin with custom model converters feature

--- springwolf-add-ons/springwolf-common-model-converters/src/main/java/io/github/stavshamir/springwolf/common_converters/configuration/CommonModelConvertersConfiguration.java
+++ springwolf-add-ons/springwolf-common-model-converters/src/main/java/io/github/stavshamir/springwolf/common_converters/configuration/CommonModelConvertersConfiguration.java
@@ -1,6 +1,6 @@
 package io.github.stavshamir.springwolf.common_converters.configuration;
 
-import io.github.stavshamir.springwolf.common_converters.converters.MonetaryAmountConverter;
+import io.github.stavshamir.springwolf.common_converters.converters.monetaryamount.MonetaryAmountConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class CommonModelConvertersConfiguration {
 
     @Bean
-    public MonetaryAmountConverter monetaryAmountConverter(){
+    public MonetaryAmountConverter monetaryAmountConverter() {
         return new MonetaryAmountConverter();
     }
 }
