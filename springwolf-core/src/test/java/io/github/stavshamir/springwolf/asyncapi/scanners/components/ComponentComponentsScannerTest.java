587ade26512bec1e462a7ae01db3ea85f8026926
Stav Shamir <shamir.stav@gmail.com>
2022-09-09 21:01:31 +0300

    Revert "Add tests that should pass when listeners in conditional beans will be scanned correctly"
    This reverts commit 3b7e7d8bf7535f5c617045138a9c4a57d210dfff.
    
    The added test does not provide the needed value for the required feature, and fail. It will be re-added when conditional beans scanning will be fixed.

--- springwolf-core/src/test/java/io/github/stavshamir/springwolf/asyncapi/scanners/components/ComponentComponentsScannerTest.java
+++ springwolf-core/src/test/java/io/github/stavshamir/springwolf/asyncapi/scanners/components/ComponentComponentsScannerTest.java
@@ -1,8 +1,6 @@
 package io.github.stavshamir.springwolf.asyncapi.scanners.components;
 
 import org.junit.Test;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 import java.util.Set;
 
@@ -28,32 +26,4 @@ public class ComponentComponentsScannerTest {
         assertThatThrownBy(() -> new ComponentComponentsScanner(""))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
-
-    @Test
-    public void scan_components_default_environment() {
-        Set<Class<?>> components = new ComponentComponentsScanner(this.getClass().getPackage().getName()).scanForComponents();
-
-        // Gives back both the @Configuration and inner @TestConfiguration classes.
-        assertThat(components)
-                .hasSize(2)
-                .contains(TestBeanConfiguration.class)
-                .contains(CustomConfiguration.class)
-                .doesNotContain(ComponentsScanner.class);
-    }
-
-    @Test
-    public void scan_components_test_environment() {
-        //ComponentComponentsScanner should provide a way to initialize profile/env
-        Set<Class<?>> components = new ComponentComponentsScanner(this.getClass().getPackage().getName()).scanForComponents();
-
-        // Gives back both the @Configuration and inner @TestConfiguration classes.
-        assertThat(components)
-                .hasSize(3)
-                .contains(TestBeanConfiguration.class)
-                .contains(CustomConfiguration.class)
-                .contains(TestBeanConditional.class)
-                .doesNotContain(ComponentsScanner.class);
-    }
-
-
 }
