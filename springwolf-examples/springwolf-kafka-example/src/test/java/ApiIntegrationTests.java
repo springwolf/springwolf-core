d581fe2dc39c20f4f3ac8183d13d9ed8931b8e24
Timon Back <timon.back+git@gmail.com>
2022-09-15 21:52:34 +0200

    test: Run ApiIntegrationTests with embeddedKafka
    Converted ApiIntegrationTests to kotlin

---
+++ springwolf-examples/springwolf-kafka-example/src/test/java/ApiIntegrationTests.java
@@ -0,0 +1,41 @@
+import io.github.stavshamir.springwolf.example.SpringwolfExampleApplication;
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringwolfExampleApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:29092", "port=29092" })
+public class ApiIntegrationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value("${server.port}")
+    public Integer serverPort;
+
+    @Test
+    public void asyncApiResourceArtifactTest() throws JSONException, IOException {
+        String url = "/springwolf/docs";
+        String actual = restTemplate.getForObject(url, String.class);
+        System.out.println("Got: " + actual);
+
+        InputStream s = this.getClass().getResourceAsStream("asyncapi.json");
+        String expected = IOUtils.toString(s, StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals(expected, actual, JSONCompareMode.NON_EXTENSIBLE);
+    }
+}
---
+++ springwolf-examples/springwolf-kafka-example/src/test/java/SpringwolfExampleApplicationTest.java
@@ -0,0 +1,24 @@
+import io.github.stavshamir.springwolf.example.SpringwolfExampleApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringwolfExampleApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:29092", "port=29092" })
+public class SpringwolfExampleApplicationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void testContext() {
+        assertNotNull(context);
+    }
+}
