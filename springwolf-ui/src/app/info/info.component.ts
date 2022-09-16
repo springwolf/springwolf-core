e45bb63958e3f35d46f5f6fd261efeb1181b4344
Timon Back <timon.back+git@gmail.com>
2022-09-16 12:49:28 +0200

    feat: Add download link for async api doc

--- springwolf-ui/src/app/info/info.component.ts
+++ springwolf-ui/src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncApi } from '../shared/models/asyncapi.model';
 import { Info } from '../shared/models/info.model';
 import { AsyncApiService } from '../shared/asyncapi.service';
 import { Subscription } from 'rxjs';
@@ -10,6 +11,7 @@ import { Subscription } from 'rxjs';
 })
 export class InfoComponent implements OnInit {
 
+  asyncApiData: AsyncApi;
   info: Info;
   nameSubscription: Subscription;
 
@@ -17,8 +19,21 @@ export class InfoComponent implements OnInit {
 
   ngOnInit(): void {
     this.nameSubscription = this.asyncApiService.getCurrentAsyncApiName().subscribe(name => {
-      this.asyncApiService.getAsyncApis().subscribe(asyncapi => this.info = asyncapi.get(name).info);
+      this.asyncApiService.getAsyncApis().subscribe(asyncapi => {
+        this.asyncApiData = asyncapi.get(name);
+        this.info = asyncapi.get(name).info;
+      });
     });
+  }
+
+  async download(): Promise<Boolean> {
+    var json = JSON.stringify(this.asyncApiData, null, 2);
+    var bytes = new TextEncoder().encode(json);
+    var blob = new Blob([bytes], { type: 'application/json' });
+    var url = window.URL.createObjectURL(blob);
+    window.open(url);
+
+    return false;
   }
 
 }
