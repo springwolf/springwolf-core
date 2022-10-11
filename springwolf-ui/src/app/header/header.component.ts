1bfb03b974d5b6a7b3881afdf479969f3e664e13
Timon Back <timon.back+git@gmail.com>
2022-10-11 23:23:15 +0200

    feat: Add header document selection to url

--- springwolf-ui/src/app/header/header.component.ts
+++ springwolf-ui/src/app/header/header.component.ts
@@ -7,16 +7,23 @@ import { AsyncApiService } from 'src/app/shared/asyncapi.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private static URL_SEARCH_DOCUMENT_PARAM = "document"
 
   documents: string[];
   selectedDocument: string;
+  location: Location;
 
-  constructor(private asyncApiService: AsyncApiService) { }
+  constructor(private asyncApiService: AsyncApiService) {
+    this.location = window.location
+    this.selectedDocument = new URLSearchParams(window.location.search).get(HeaderComponent.URL_SEARCH_DOCUMENT_PARAM)
+  }
 
   ngOnInit(): void {
     this.asyncApiService.getAsyncApis().subscribe(docsMap => {
       this.documents = [...docsMap.keys()];
-      this.selectedDocument = this.documents[0]
+      if(!this.documents.includes(this.selectedDocument)) {
+        this.selectedDocument = this.documents[0]
+      }
       this.asyncApiService.setCurrentAsyncApiName(this.selectedDocument);
     });
   }
@@ -26,8 +33,9 @@ export class HeaderComponent implements OnInit {
   }
 
   set selectedDocumentMod(value) {
-    this.selectedDocument = value;
-    this.asyncApiService.setCurrentAsyncApiName(this.selectedDocument);
+    let updatedSearchParams = new URLSearchParams(window.location.search)
+    updatedSearchParams.set(HeaderComponent.URL_SEARCH_DOCUMENT_PARAM, value)
+    this.location.search = updatedSearchParams.toString()
   }
 
 }
