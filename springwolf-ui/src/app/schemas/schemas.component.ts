cdbf16898eda024c4c4d42df43495f762243d860
Timon Back <timon.back+git@gmail.com>
2022-10-11 22:57:52 +0200

    feat: Set up links for schema definitions

--- springwolf-ui/src/app/schemas/schemas.component.ts
+++ springwolf-ui/src/app/schemas/schemas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import { Subscription, SubscriptionLike } from 'rxjs';
 import { AsyncApiService } from '../shared/asyncapi.service';
 import { Schema } from '../shared/models/schema.model';
@@ -12,14 +13,27 @@ export class SchemasComponent implements OnInit {
 
 
   schemas: Map<string, Schema>;
+  selectedSchema: string;
   nameSubscription: Subscription;
 
-  constructor(private asyncApiService: AsyncApiService) { }
+  constructor(private asyncApiService: AsyncApiService, private location: Location) {
+    this.setSchemaSelectionFromLocation()
+  }
 
   ngOnInit(): void {
+    this.location.subscribe(() : void => this.setSchemaSelectionFromLocation())
+
     this.nameSubscription = this.asyncApiService.getCurrentAsyncApiName().subscribe(name => {
       this.asyncApiService.getAsyncApis().subscribe(asyncapi => this.schemas = asyncapi.get(name).components.schemas);
     });
   }
 
+  setSchemaSelectionFromLocation(): void {
+    this.selectedSchema = window.location.hash.substr(1);
+  }
+  resetSchemaSelection(currentSchema: string): void {
+    if(this.selectedSchema == currentSchema) {
+      this.selectedSchema = undefined
+    }
+  }
 }
