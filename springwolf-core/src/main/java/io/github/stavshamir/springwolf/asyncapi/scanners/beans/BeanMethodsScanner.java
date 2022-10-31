7a161784b63daeee97cfda4050f3a63641dad9be
Stav Shamir <shamir.stav@gmail.com>
2022-10-31 20:02:10 +0200

    Add BeanMethodsScanner

--- springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/beans/BeanMethodsScanner.java
+++ springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/beans/BeanMethodsScanner.java
@@ -1,55 +1,13 @@
 package io.github.stavshamir.springwolf.asyncapi.scanners.beans;
 
-import io.github.stavshamir.springwolf.configuration.AsyncApiDocket;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.stereotype.Service;
-
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toSet;
+public interface BeanMethodsScanner {
 
-@Service
-@RequiredArgsConstructor
-public class BeanMethodsScanner {
-
-    private final AsyncApiDocket docket;
-
-    private Optional<Class<?>> getClass(String className) {
-        try {
-            return Optional.of(Class.forName(className));
-        } catch (ClassNotFoundException e) {
-            return Optional.empty();
-        }
-    }
-
-    public Set<Method> getBeanMethods() {
-        return getConfigurationClasses()
-                .map(Class::getDeclaredMethods)
-                .map(Arrays::asList)
-                .flatMap(List::stream)
-                .filter(method -> method.isAnnotationPresent(Bean.class))
-                .collect(toSet());
-    }
-
-    private Stream<? extends Class<?>> getConfigurationClasses() {
-        ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
-        provider.addIncludeFilter(new AnnotationTypeFilter(Configuration.class));
-
-        return provider.findCandidateComponents("").stream()
-                .map(BeanDefinition::getBeanClassName)
-                .map(this::getClass)
-                .filter(Optional::isPresent)
-                .map(Optional::get);
-    }
+    /**
+     * @return Methods annotated with @Bean which are declared in @Configuration classes from the base package.
+     */
+    Set<Method> getBeanMethods();
 
 }
---
+++ springwolf-core/src/test/java/io/github/stavshamir/springwolf/asyncapi/scanners/beans/DefaultBeanMethodsScannerTest.java
@@ -0,0 +1,63 @@
+package io.github.stavshamir.springwolf.asyncapi.scanners.beans;
+
+import com.google.common.collect.ImmutableSet;
+import io.github.stavshamir.springwolf.asyncapi.scanners.classes.ConfigurationClassScanner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {DefaultBeanMethodsScanner.class})
+public class DefaultBeanMethodsScannerTest {
+
+    @Autowired
+    private DefaultBeanMethodsScanner beanMethodsScanner;
+
+    @MockBean
+    private ConfigurationClassScanner configurationClassScanner;
+
+    @Test
+    public void name() {
+        when(configurationClassScanner.scan())
+                .thenReturn(ImmutableSet.of(ConfigurationClass.class));
+
+        Set<String> beanMethods = beanMethodsScanner.getBeanMethods().stream()
+                .map(Method::getName)
+                .collect(Collectors.toSet());
+
+
+        assertThat(beanMethods)
+                .hasSize(2)
+                .containsExactlyInAnyOrder("stringBean", "consumerBean");
+    }
+
+    private static class ConfigurationClass {
+
+        private void notABean() {
+        }
+
+        @Bean
+        private String stringBean() {
+            return "foo";
+        }
+
+        @Bean
+        private Consumer<String> consumerBean() {
+            return System.out::println;
+        }
+
+    }
+
+}
