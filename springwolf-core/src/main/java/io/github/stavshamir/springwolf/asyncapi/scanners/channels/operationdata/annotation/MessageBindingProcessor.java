f60a340bed43b6dbcd79060f13b6d41fa84209d0
David Müller <d4ve.mone@googlemail.com>
2023-01-16 10:14:39 +0100

    Fix java doc of MessageBindingProcessor after rebase

--- springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/annotation/MessageBindingProcessor.java
+++ springwolf-core/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/annotation/MessageBindingProcessor.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface MessageBindingProcessor {
 
     /**
-     * Process the methods annotated with {@link AsyncPublisher} and {@link AsyncSubscriber}
+     * Process the methods annotated with {@link AsyncPublisher} and {@link AsyncListener}
      * for protocol specific messageBinding annotations, method parameters, etc
      *
      * @param method The method being annotated
