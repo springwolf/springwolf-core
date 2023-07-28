7872fb6cf53c9a167c07d464e85d7c2a09f42c57
David Müller <d4ve.mone@googlemail.com>
2023-07-28 14:37:53 +0200

    refactor: Extract AsyncApiDocketFixture
    Co-authored-by: Timon Back <timonback@users.noreply.github.com>

--- springwolf-core/src/test/java/io/github/stavshamir/springwolf/asyncapi/DefaultAsyncApiServiceTest.java
+++ springwolf-core/src/test/java/io/github/stavshamir/springwolf/asyncapi/DefaultAsyncApiServiceTest.java
@@ -1,10 +1,9 @@
 package io.github.stavshamir.springwolf.asyncapi;
 
-import com.asyncapi.v2._6_0.model.info.Info;
-import com.asyncapi.v2._6_0.model.server.Server;
 import io.github.stavshamir.springwolf.asyncapi.types.AsyncAPI;
 import io.github.stavshamir.springwolf.configuration.AsyncApiDocket;
 import io.github.stavshamir.springwolf.configuration.AsyncApiDocketService;
+import io.github.stavshamir.springwolf.fixtures.AsyncApiDocketFixture;
 import io.github.stavshamir.springwolf.schemas.SchemasService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -45,7 +44,7 @@ public class DefaultAsyncApiServiceTest {
     @Test
     void shouldThrowExceptionOnSubsequentGetAsyncApi() {
         // Given an AsyncApiDocketService which throws an exception on 'getAsyncApiDocket()'
-        prepareExceptionDocket();
+        when(asyncApiDocketService.getAsyncApiDocket()).thenThrow(new RuntimeException("test exception"));
 
         // When getAsyncAPI is invoked the first time
         Throwable cause = null;
@@ -74,7 +73,8 @@ public class DefaultAsyncApiServiceTest {
     @Test
     void shouldLazyInitializeOnLoadingModeLazy() {
         // Given a valid Docket-Specification
-        prepareValidDocket();
+        AsyncApiDocket docket = AsyncApiDocketFixture.createMinimal();
+        when(asyncApiDocketService.getAsyncApiDocket()).thenReturn(docket);
 
         // When getAsyncAPI() isn't called yet
         // Then isNotInitilized() returns 'true'.
@@ -87,38 +87,5 @@ public class DefaultAsyncApiServiceTest {
         // isNotInitialized() return 'false'.
         assertThat(asyncAPI).isNotNull();
         assertThat(defaultAsyncApiService.isNotInitialized()).isFalse();
-    }
-
-    /**
-     * prepares prepares {@link AsyncApiDocketService} mock to throw Exception on getAsyncApiDocket().
-     */
-    private void prepareExceptionDocket() {
-        when(asyncApiDocketService.getAsyncApiDocket()).thenThrow(new RuntimeException("test exception"));
-    }
-
-    /**
-     * prepares {@link AsyncApiDocketService} mock to return a valid AsyncApiDocket.
-     */
-    private void prepareValidDocket() {
-        Info info = Info.builder()
-                .description("Info")
-                .version("1.0")
-                .title("Test AsyncApi")
-                .build();
-
-        Server server1 = Server.builder()
-                .description("Server 1")
-                .url("mq:/localhost:1234")
-                .protocol("kafka")
-                .build();
-
-        AsyncApiDocket docket = AsyncApiDocket.builder()
-                .id("id")
-                .defaultContentType("application/json")
-                .servers(Map.of("server", server1))
-                .info(info)
-                .build();
-
-        when(asyncApiDocketService.getAsyncApiDocket()).thenReturn(docket);
     }
 }
