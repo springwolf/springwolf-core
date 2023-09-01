ef3771c1b478d41473c453d8bf725b4703959a37
Timon Back <timon.back@otto.de>
2023-09-01 14:52:45 +0200

    chore(sqs): Use AsyncOperationBinding code from master

--- springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/SqsOperationBindingProcessor.java
+++ springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/SqsOperationBindingProcessor.java
@@ -1,35 +1,15 @@
 package io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata;
 
 import com.asyncapi.v2.binding.operation.sqs.SQSOperationBinding;
-import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.OperationBindingProcessor;
+import io.github.stavshamir.springwolf.asyncapi.scanners.channels.AbstractOperationBindingProcessor;
 import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.SqsAsyncOperationBinding;
-import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringValueResolver;
-
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Optional;
 
 @Component
-public class SqsOperationBindingProcessor implements OperationBindingProcessor, EmbeddedValueResolverAware {
-    private StringValueResolver resolver;
+public class SqsOperationBindingProcessor extends AbstractOperationBindingProcessor<SqsAsyncOperationBinding> {
 
     @Override
-    public void setEmbeddedValueResolver(StringValueResolver resolver) {
-        this.resolver = resolver;
-    }
-
-    @Override
-    public Optional<ProcessedOperationBinding> process(Method method) {
-        return Arrays.stream(method.getAnnotations())
-                .filter(annotation -> annotation instanceof SqsAsyncOperationBinding)
-                .map(annotation -> (SqsAsyncOperationBinding) annotation)
-                .findAny()
-                .map(this::mapToOperationBinding);
-    }
-
-    private ProcessedOperationBinding mapToOperationBinding(SqsAsyncOperationBinding bindingAnnotation) {
+    protected ProcessedOperationBinding mapToOperationBinding(SqsAsyncOperationBinding bindingAnnotation) {
         return new ProcessedOperationBinding(bindingAnnotation.type(), new SQSOperationBinding());
     }
 }
