be444d57545510bb064b87b58a0fb8fe48769297
Timon Back <timon.back@otto.de>
2023-09-29 16:13:56 +0200

    refactor(plugin): avoid user controlled input

--- springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/controller/SpringwolfSqsController.java
+++ springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/controller/SpringwolfSqsController.java
@@ -4,8 +4,9 @@ package io.github.stavshamir.springwolf.asyncapi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.stavshamir.springwolf.asyncapi.controller.dtos.MessageDto;
-import io.github.stavshamir.springwolf.configuration.AsyncApiDocketService;
 import io.github.stavshamir.springwolf.producer.SpringwolfSqsProducer;
+import io.github.stavshamir.springwolf.schemas.DefaultSchemasService;
+import io.swagger.v3.oas.models.media.Schema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
@@ -25,7 +26,7 @@ import java.text.MessageFormat;
 @RequiredArgsConstructor
 public class SpringwolfSqsController implements InitializingBean {
 
-    private final AsyncApiDocketService asyncApiDocketService;
+    private final DefaultSchemasService schemasService;
 
     private final SpringwolfSqsProducer producer;
 
@@ -34,26 +35,41 @@ public class SpringwolfSqsController implements InitializingBean {
     @PostMapping("/publish")
     public void publish(@RequestParam String topic, @RequestBody MessageDto message) {
         if (!producer.isEnabled()) {
-            log.warn("SQS producer is not enabled - message will not be published");
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "SQS producer is not enabled");
+            log.warn("Kafka producer is not enabled - message will not be published");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Kafka producer is not enabled");
         }
 
-        String payloadType = message.getPayloadType();
-        if (payloadType.startsWith(asyncApiDocketService.getAsyncApiDocket().getBasePackage())) {
-            try {
-                Class<?> payloadClass = Class.forName(payloadType);
-                Object payload = objectMapper.readValue(message.getPayload(), payloadClass);
+        boolean foundDefinition = false;
+        String messagePayloadType = message.getPayloadType();
+        for (Schema<?> value : schemasService.getDefinitions().values()) {
+            String schemaPayloadType = value.getName();
+            // security: match against user input, but always use our controlled data from the DefaultSchemaService
+            if (schemaPayloadType.equals(messagePayloadType)) {
+                publishMessage(topic, message, schemaPayloadType);
 
-                log.debug("Publishing to sqs queue {}: {}", topic, payload);
-                producer.send(topic, payload);
-            } catch (ClassNotFoundException | JsonProcessingException ex) {
-                throw new ResponseStatusException(
-                        HttpStatus.BAD_REQUEST,
-                        MessageFormat.format(
-                                "Unable to create payload {0} from data: {1}", payloadType, message.getPayload()));
+                foundDefinition = true;
+                break;
             }
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No payloadType specified.");
+        }
+
+        if (!foundDefinition) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Specified payloadType is not a registered springwolf schema.");
+        }
+    }
+
+    private void publishMessage(String topic, MessageDto message, String schemaPayloadType) {
+        try {
+            Class<?> payloadClass = Class.forName(schemaPayloadType);
+            Object payload = objectMapper.readValue(message.getPayload(), payloadClass);
+
+            log.debug("Publishing to SQS queue {}: {}", topic, message);
+            producer.send(topic, payload);
+        } catch (ClassNotFoundException | JsonProcessingException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    MessageFormat.format(
+                            "Unable to create payload {0} from data: {1}", schemaPayloadType, message.getPayload()));
         }
     }
 
