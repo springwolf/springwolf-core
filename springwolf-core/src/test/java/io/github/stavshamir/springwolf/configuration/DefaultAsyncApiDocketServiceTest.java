70c9e35d3f485b7bbf314b130f3b3cb399dfd687
thomas <thomas@vahrst.de>
2023-10-21 08:46:54 +0200

    test caching of dockets in DefaultAsyncApiDocketServiceTest.java
    Took 6 hours 16 minutes

--- springwolf-core/src/test/java/io/github/stavshamir/springwolf/configuration/DefaultAsyncApiDocketServiceTest.java
+++ springwolf-core/src/test/java/io/github/stavshamir/springwolf/configuration/DefaultAsyncApiDocketServiceTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.util.Maps.newHashMap;
 class DefaultAsyncApiDocketServiceTest {
 
     @Test
-    void testConfigurationShouldMapAllPropertiesToTheDocket() {
+    void testServiceShouldMapAllPropertiesToTheDocket() {
         // given
         ConfigDocket configDocket = new ConfigDocket();
         configDocket.setBasePackage("test-base-package");
@@ -40,9 +40,8 @@ class DefaultAsyncApiDocketServiceTest {
         properties.setDocket(configDocket);
 
         // when
-        DefaultAsyncApiDocketService docketConfiguration =
-                new DefaultAsyncApiDocketService(Optional.empty(), properties);
-        AsyncApiDocket asyncApiDocket = docketConfiguration.getAsyncApiDocket();
+        DefaultAsyncApiDocketService docketService = new DefaultAsyncApiDocketService(Optional.empty(), properties);
+        AsyncApiDocket asyncApiDocket = docketService.getAsyncApiDocket();
 
         // then
         assertThat(asyncApiDocket.getDefaultContentType()).isEqualTo(configDocket.getDefaultContentType());
@@ -52,5 +51,62 @@ class DefaultAsyncApiDocketServiceTest {
         assertThat(asyncApiDocket.getInfo().getDescription()).isEqualTo(info.getDescription());
         assertThat(asyncApiDocket.getInfo().getLicense()).isEqualTo(info.getLicense());
         assertThat(asyncApiDocket.getInfo().getContact()).isEqualTo(info.getContact());
+    }
+
+    @Test
+    void docketServiceShouldDeliverCachedConfigDocketBean() {
+        // repeatable invocations of AsyncApiDocketService.getAsyncApiDocket() should return the same docket instance
+        // if a ConfigDocket @Bean is present.
+
+        // given
+        AsyncApiDocket customDocket = AsyncApiDocket.builder()
+                .basePackage("test-base-package")
+                .info(com.asyncapi.v2._6_0.model.info.Info.builder()
+                        .title("some-title")
+                        .version("some-version")
+                        .build())
+                .build();
+
+        SpringwolfConfigProperties configProperties = new SpringwolfConfigProperties();
+
+        // when
+        AsyncApiDocketService docketService =
+                new DefaultAsyncApiDocketService(Optional.of(customDocket), configProperties);
+
+        // then
+        assertThat(docketService.getAsyncApiDocket()).isSameAs(customDocket);
+        // second invocation should again return same instance
+        assertThat(docketService.getAsyncApiDocket()).isSameAs(customDocket);
+    }
+
+    @Test
+    void docketServiceShouldDeliverCachedEnvironmentBasedDocket() {
+        // repeatable invocations of AsyncApiDocketService.getAsyncApiDocket() should return the same docket instance
+        // if docket is based on environment properties.
+
+        // given
+        ConfigDocket configDocket = new ConfigDocket();
+        configDocket.setBasePackage("test-base-package");
+        configDocket.setDefaultContentType("application/json");
+
+        Info info = new Info();
+        info.setTitle("some-title");
+        info.setVersion("some-version");
+        configDocket.setInfo(info);
+
+        Server server =
+                Server.builder().protocol("some-protocol").url("some-url").build();
+        configDocket.setServers(newHashMap("some-protocol", server));
+
+        SpringwolfConfigProperties configProperties = new SpringwolfConfigProperties();
+        configProperties.setDocket(configDocket);
+
+        // when
+        AsyncApiDocketService docketService = new DefaultAsyncApiDocketService(Optional.empty(), configProperties);
+        AsyncApiDocket asyncApiDocket = docketService.getAsyncApiDocket();
+
+        // then
+        // second invocation should again return same instance
+        assertThat(docketService.getAsyncApiDocket()).isSameAs(asyncApiDocket);
     }
 }
