68706c37c9efb9426bf8bb39e8c9d83afe25e3a2
Krzysztof Kwiecień <krzysztofxkwiecien@gmail.com>
2023-11-05 14:10:59 +0100

    fix formatting with spotlessApply

--- springwolf-plugins/springwolf-jms-plugin/src/main/java/io/github/stavshamir/springwolf/configuration/properties/SpringwolfJmsConfigConstants.java
+++ springwolf-plugins/springwolf-jms-plugin/src/main/java/io/github/stavshamir/springwolf/configuration/properties/SpringwolfJmsConfigConstants.java
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package io.github.stavshamir.springwolf.configuration.properties;
 
-import static io.github.stavshamir.springwolf.configuration.properties.SpringwolfConfigConstants.*;
+import static io.github.stavshamir.springwolf.configuration.properties.SpringwolfConfigConstants.ENABLED;
+import static io.github.stavshamir.springwolf.configuration.properties.SpringwolfConfigConstants.SCANNER;
+import static io.github.stavshamir.springwolf.configuration.properties.SpringwolfConfigConstants.SPRINGWOLF_PLUGIN_CONFIG_PREFIX;
 
 public class SpringwolfJmsConfigConstants {
 
