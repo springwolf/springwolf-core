082a4d925b39175918a002dcc81ac2422be7fd81
Ali Evren Küt <alievren.kut@t2.com.tr>
2023-12-29 09:10:20 +0300

    springwolf kafka protobuf example has been implemented.

---
+++ springwolf-examples/springwolf-kafka-protobuf-example/src/main/java/io/github/stavshamir/springwolf/example/kafka/protobuf/consumer/ProtobufSpringConsumer.java
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Apache-2.0
+package io.github.stavshamir.springwolf.example.kafka.protobuf.consumer;
+
+import com.google.protobuf.DynamicMessage;
+import com.google.protobuf.InvalidProtocolBufferException;
+import io.confluent.protobuf.entity.CustomerMessagePayload;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProtobufSpringConsumer {
+
+    private final Logger logger = LoggerFactory.getLogger(ProtobufSpringConsumer.class);
+    private static final String TOPIC = "protobuf-topic";
+
+    @KafkaListener(topics = TOPIC, groupId = "group_id_v1")
+    public void processEvent(CustomerMessagePayload.CustomerMessage message) {
+        logger.info(String.format("#### -> Consuming message -> %s", message.toString()));
+    }
+}
