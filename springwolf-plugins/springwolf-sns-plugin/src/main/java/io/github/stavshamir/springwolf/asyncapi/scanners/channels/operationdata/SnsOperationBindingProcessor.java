31d5b60d0bf4fd0588e972ec25867fe59c1c6cc3
Carlos Tasada <ctasada@gmail.com>
2024-01-15 17:26:55 +0100

    fix: Minor fix to sns-plugin spec example
    The asyncapi test sample had a minor issue when validated with https://studio.asyncapi.com/
    
    We need to properly configure those values, something that I will take care of in the next PR

--- springwolf-plugins/springwolf-sns-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/SnsOperationBindingProcessor.java
+++ springwolf-plugins/springwolf-sns-plugin/src/main/java/io/github/stavshamir/springwolf/asyncapi/scanners/channels/operationdata/SnsOperationBindingProcessor.java
@@ -5,11 +5,24 @@ import io.github.stavshamir.springwolf.asyncapi.scanners.bindings.ProcessedOpera
 import io.github.stavshamir.springwolf.asyncapi.scanners.bindings.processor.AbstractOperationBindingProcessor;
 import io.github.stavshamir.springwolf.asyncapi.scanners.channels.operationdata.annotation.SnsAsyncOperationBinding;
 import io.github.stavshamir.springwolf.asyncapi.v3.bindings.sns.SNSOperationBinding;
+import io.github.stavshamir.springwolf.asyncapi.v3.bindings.sns.SNSOperationBindingConsumer;
+import io.github.stavshamir.springwolf.asyncapi.v3.bindings.sns.SNSOperationBindingIdentifier;
+
+import java.util.List;
 
 public class SnsOperationBindingProcessor extends AbstractOperationBindingProcessor<SnsAsyncOperationBinding> {
 
     @Override
     protected ProcessedOperationBinding mapToOperationBinding(SnsAsyncOperationBinding bindingAnnotation) {
-        return new ProcessedOperationBinding(bindingAnnotation.type(), new SNSOperationBinding());
+        // FIXME: Read these values from the annotation
+        var consumer = SNSOperationBindingConsumer.builder()
+                .protocol(SNSOperationBindingConsumer.Protocol.SQS)
+                .endpoint(SNSOperationBindingIdentifier.builder().build())
+                .rawMessageDelivery(true)
+                .build();
+        var snsOperationBinding = SNSOperationBinding.builder()
+                .consumers(List.of(consumer))
+                .build();
+        return new ProcessedOperationBinding(bindingAnnotation.type(), snsOperationBinding);
     }
 }
