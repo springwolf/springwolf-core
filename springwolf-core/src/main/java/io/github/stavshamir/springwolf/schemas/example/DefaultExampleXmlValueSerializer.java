895f721c9409f498c073854cbc9cee28c27e1be9
David Müller <d4ve.mone@googlemail.com>
2024-02-23 16:52:36 +0100

    refactor(core): minimal serialization configuration for DefaultExampleXmlValueSerializer
    Co-authored-by: Timon Back <timonback@users.noreply.github.com>

--- springwolf-core/src/main/java/io/github/stavshamir/springwolf/schemas/example/DefaultExampleXmlValueSerializer.java
+++ springwolf-core/src/main/java/io/github/stavshamir/springwolf/schemas/example/DefaultExampleXmlValueSerializer.java
@@ -19,8 +19,6 @@ public class DefaultExampleXmlValueSerializer implements ExampleXmlValueSerializ
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
         transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-        transformer.setOutputProperty(OutputKeys.ENCODING, "ISO-8859-1");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
         transformer.setOutputProperty(OutputKeys.INDENT, "no");
         StringWriter sw = new StringWriter();
         StreamResult sr = new StreamResult(sw);
