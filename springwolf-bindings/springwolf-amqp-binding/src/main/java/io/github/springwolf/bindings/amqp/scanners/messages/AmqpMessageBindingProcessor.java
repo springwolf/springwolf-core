b0b78361d1eb39567803831b0d9f4e5c46a07ae5
Timon Back <timonback@users.noreply.github.com>
2024-04-27 21:51:11 +0200

    feat: add amqp bindings artifact

---
+++ springwolf-bindings/springwolf-amqp-binding/src/main/java/io/github/springwolf/bindings/amqp/scanners/messages/AmqpMessageBindingProcessor.java
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+package io.github.springwolf.bindings.amqp.scanners.messages;
+
+import io.github.springwolf.asyncapi.v3.bindings.amqp.AMQPMessageBinding;
+import io.github.springwolf.bindings.amqp.annotations.AmqpAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.scanners.bindings.messages.MessageBindingProcessor;
+import io.github.springwolf.core.asyncapi.scanners.bindings.messages.ProcessedMessageBinding;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.util.StringValueResolver;
+
+import java.lang.reflect.AnnotatedElement;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class AmqpMessageBindingProcessor implements MessageBindingProcessor, EmbeddedValueResolverAware {
+    private StringValueResolver resolver;
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public Optional<ProcessedMessageBinding> process(AnnotatedElement annotatedElement) {
+        return Arrays.stream(annotatedElement.getAnnotations())
+                .filter(AmqpAsyncOperationBinding.class::isInstance)
+                .map(AmqpAsyncOperationBinding.class::cast)
+                .findAny()
+                .map(this::mapToMessageBinding);
+    }
+
+    private ProcessedMessageBinding mapToMessageBinding(AmqpAsyncOperationBinding bindingAnnotation) {
+        AMQPMessageBinding amqpMessageBinding = AMQPMessageBinding.builder().build();
+
+        return new ProcessedMessageBinding("amqp", amqpMessageBinding);
+    }
+}
---
+++ springwolf-bindings/springwolf-amqp-binding/src/main/java/io/github/springwolf/bindings/amqp/scanners/operations/AmqpOperationBindingProcessor.java
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+package io.github.springwolf.bindings.amqp.scanners.operations;
+
+import io.github.springwolf.asyncapi.v3.bindings.amqp.AMQPOperationBinding;
+import io.github.springwolf.bindings.amqp.annotations.AmqpAsyncOperationBinding;
+import io.github.springwolf.core.asyncapi.scanners.bindings.operations.AbstractOperationBindingProcessor;
+import io.github.springwolf.core.asyncapi.scanners.bindings.operations.ProcessedOperationBinding;
+
+import java.util.Arrays;
+
+public class AmqpOperationBindingProcessor extends AbstractOperationBindingProcessor<AmqpAsyncOperationBinding> {
+    @Override
+    protected ProcessedOperationBinding mapToOperationBinding(AmqpAsyncOperationBinding bindingAnnotation) {
+        AMQPOperationBinding amqpOperationBinding = AMQPOperationBinding.builder()
+                .expiration(bindingAnnotation.expiration())
+                .cc(Arrays.stream(bindingAnnotation.cc())
+                        .map(this::resolveOrNull)
+                        .toList())
+                .priority(bindingAnnotation.priority())
+                .deliveryMode(bindingAnnotation.deliveryMode())
+                .mandatory(bindingAnnotation.mandatory())
+                .timestamp(bindingAnnotation.timestamp())
+                .ack(bindingAnnotation.ack())
+                .build();
+
+        return new ProcessedOperationBinding("amqp", amqpOperationBinding);
+    }
+}
