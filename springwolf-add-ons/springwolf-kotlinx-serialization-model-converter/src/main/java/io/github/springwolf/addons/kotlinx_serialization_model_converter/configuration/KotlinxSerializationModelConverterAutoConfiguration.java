70d8f2ddebc04c93fb972be4aa2f060ab89ff7a8
Carlos Tasada <ctasada@gmail.com>
2024-04-29 18:12:55 +0200

    (fix) Fixed a bug loading the Kotlinx Module without `use-fqn` variable
    When the Kotlinx Serialization Module was available in an environment without the `springwolf.use-fqn` the code was failing, since it was trying to inject the variable.
    
    The code is now loading the converter **only** if `springwolf.enabled` is `true` (default value). The `springwolf.use-fqn` is also defaulting to `true`.
    
    This solution fixes the identified issue.

--- springwolf-add-ons/springwolf-kotlinx-serialization-model-converter/src/main/java/io/github/springwolf/addons/kotlinx_serialization_model_converter/configuration/KotlinxSerializationModelConverterAutoConfiguration.java
+++ springwolf-add-ons/springwolf-kotlinx-serialization-model-converter/src/main/java/io/github/springwolf/addons/kotlinx_serialization_model_converter/configuration/KotlinxSerializationModelConverterAutoConfiguration.java
@@ -2,19 +2,24 @@
 package io.github.springwolf.addons.kotlinx_serialization_model_converter.configuration;
 
 import io.github.springwolf.addons.kotlinx_serialization_model_converter.converter.KotlinxSerializationModelConverter;
+import io.github.springwolf.core.configuration.properties.SpringwolfConfigConstants;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static io.github.springwolf.core.configuration.properties.SpringwolfConfigConstants.SPRINGWOLF_USE_FQN;
 
 /**
  * Spring AutoConfiguration adding an {@link KotlinxSerializationModelConverter} Bean to the spring context.
  */
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty(name = SpringwolfConfigConstants.SPRINGWOLF_ENABLED, havingValue = "true", matchIfMissing = true)
 public class KotlinxSerializationModelConverterAutoConfiguration {
 
     @Bean
     public KotlinxSerializationModelConverter kotlinxSerializationTypeConverter(
-            @Value("${springwolf.use-fqn}") boolean useFqn) {
+            @Value("${" + SPRINGWOLF_USE_FQN + ":true}") boolean useFqn) {
         return new KotlinxSerializationModelConverter(useFqn);
     }
 }
