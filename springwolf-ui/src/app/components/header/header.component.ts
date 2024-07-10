c6085e02e9b4980ca10b7b50dfc89e3c723f6290
Timon Back <timonback@users.noreply.github.com>
2024-07-10 23:40:01 +0200

    feat(ui): update header

--- springwolf-ui/src/app/components/header/header.component.ts
+++ springwolf-ui/src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 import { Component, OnInit } from "@angular/core";
 import { UiService } from "../../service/ui.service";
+import { AsyncApiService } from "../../service/asyncapi/asyncapi.service";
 
 @Component({
   selector: "app-header",
@@ -9,11 +10,19 @@ import { UiService } from "../../service/ui.service";
 })
 export class HeaderComponent implements OnInit {
   isNewUi: boolean = true;
+  title: string = "";
 
-  constructor(private uiService: UiService) {}
+  constructor(
+    private uiService: UiService,
+    private asyncApiService: AsyncApiService
+  ) {}
 
   ngOnInit() {
     this.uiService.isNewUi$.subscribe((value) => (this.isNewUi = value));
+
+    this.asyncApiService.getAsyncApi().subscribe((asyncapi) => {
+      this.title = asyncapi.info.title;
+    });
   }
 
   toggleIsNewUi(value: boolean) {
