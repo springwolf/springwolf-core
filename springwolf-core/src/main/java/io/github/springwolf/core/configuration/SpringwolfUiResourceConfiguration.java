c4da81d7c82f406702349bc57456fa4c6ce903e6
sam0r040 <93372330+sam0r040@users.noreply.github.com>
2024-07-26 15:55:23 +0200

    fix(core): Fix startup when Spring WebMVC is not used (#868)
    Only use SpringwolfUiResourceConfiguration if the required beans are present in the spring context (WebMvcProperties, WebProperties)
    
    Co-authored-by: Timon Back <timonback@users.noreply.github.com>

--- springwolf-core/src/main/java/io/github/springwolf/core/configuration/SpringwolfUiResourceConfiguration.java
+++ springwolf-core/src/main/java/io/github/springwolf/core/configuration/SpringwolfUiResourceConfiguration.java
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: Apache-2.0
 package io.github.springwolf.core.configuration;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ConditionalOnClass(WebMvcConfigurer.class)
+@ConditionalOnBean({WebMvcProperties.class, WebProperties.class})
 @Import(SpringwolfUiResourceConfigurer.class)
 public class SpringwolfUiResourceConfiguration {}
