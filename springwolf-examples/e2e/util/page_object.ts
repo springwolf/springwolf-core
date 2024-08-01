dbbb833f31f20f79dae7d68446f6b3fb2db7061a
Timon Back <timonback@users.noreply.github.com>
2024-08-02 00:09:01 +0200

    feat(ui): make new-ui the default (#877)

--- springwolf-examples/e2e/util/page_object.ts
+++ springwolf-examples/e2e/util/page_object.ts
@@ -2,13 +2,11 @@
 import { Locator, Page } from "@playwright/test";
 
 export function locateChannels(locator: Page | Locator) {
-  return locator.locator("app-channels > mat-accordion");
-  // return locator.locator("section#channels article"); // for new ui
+  return locator.locator("section#channels article");
 }
 
 export function locateChannelItems(locator: Page | Locator) {
-  return locator.locator("app-channels > mat-accordion > mat-expansion-panel");
-  // return locator.locator("section#channels article > mat-card"); // for new ui
+  return locator.locator("section#channels article > mat-card");
 }
 
 export function locateChannel(
@@ -18,9 +16,8 @@ export function locateChannel(
   action: string,
   payload: string
 ) {
-  return locator.getByTestId(
-    // return locator.locator( // for new ui
-    "channel-" + protocol + "-" + channelName + "-" + action + "-" + payload
+  return locator.locator(
+    "#channel-" + protocol + "-" + channelName + "-" + action + "-" + payload
   );
 }
 
