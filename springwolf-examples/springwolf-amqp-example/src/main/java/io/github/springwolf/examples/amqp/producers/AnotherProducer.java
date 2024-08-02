2c3cd54c2f55492114c3228f84c4468ecdb8f36c
Heribert Hirth <hirth.heribert@audi.de>
2024-08-02 13:56:55 +0200

    Example for header override bug

--- springwolf-examples/springwolf-amqp-example/src/main/java/io/github/springwolf/examples/amqp/producers/AnotherProducer.java
+++ springwolf-examples/springwolf-amqp-example/src/main/java/io/github/springwolf/examples/amqp/producers/AnotherProducer.java
@@ -3,8 +3,11 @@ package io.github.springwolf.examples.amqp.producers;
 
 import io.github.springwolf.bindings.amqp.annotations.AmqpAsyncOperationBinding;
 import io.github.springwolf.core.asyncapi.annotations.AsyncOperation;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation.Headers;
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation.Headers.Header;
 import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher;
 import io.github.springwolf.examples.amqp.dtos.AnotherPayloadDto;
+import io.github.springwolf.examples.amqp.dtos.ExamplePayloadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
@@ -18,9 +21,22 @@ public class AnotherProducer {
             operation =
                     @AsyncOperation(
                             channelName = "example-topic-exchange",
-                            description = "Custom, optional description defined in the AsyncPublisher annotation"))
+                            description = "Custom, optional description defined in the AsyncPublisher annotation",
+                            headers = @Headers(values = @Header(name = "some-header", value = "some-value"))))
     @AmqpAsyncOperationBinding()
     public void sendMessage(AnotherPayloadDto msg) {
+        // send
+        rabbitTemplate.convertAndSend("example-topic-exchange", "example-topic-routing-key", msg);
+    }
+
+    @AsyncPublisher(
+            operation =
+            @AsyncOperation(
+                    channelName = "example-topic-exchange",
+                    description = "Custom, optional description defined in the AsyncPublisher annotation",
+                    headers = @Headers(values = @Header(name = "some-header", value = "another-value"))))
+    @AmqpAsyncOperationBinding()
+    public void sendMessage(ExamplePayloadDto msg) {
         // send
         rabbitTemplate.convertAndSend("example-topic-exchange", "example-topic-routing-key", msg);
     }
