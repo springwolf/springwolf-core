4ff9766a454ab30a2b9013d7640b2d1f4cc3e793
Timon Back <timonback@users.noreply.github.com>
2024-08-03 15:47:45 +0200

    chore(cloudstream): update example
    re-activate the kafka binding

--- springwolf-examples/springwolf-cloud-stream-example/src/main/java/io/github/springwolf/examples/cloudstream/configuration/CloudstreamConfiguration.java
+++ springwolf-examples/springwolf-cloud-stream-example/src/main/java/io/github/springwolf/examples/cloudstream/configuration/CloudstreamConfiguration.java
@@ -9,7 +9,6 @@ import io.github.springwolf.examples.cloudstream.dtos.AnotherPayloadDto;
 import io.github.springwolf.examples.cloudstream.dtos.ExamplePayloadDto;
 import io.github.springwolf.examples.cloudstream.dtos.GooglePubSubPayloadDto;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,10 +19,13 @@ import java.util.function.Function;
 @Configuration
 public class CloudstreamConfiguration {
     @Bean
-    public Function<KStream<Void, ExamplePayloadDto>, KStream<Void, AnotherPayloadDto>> process() {
-        return input -> input.peek((k, v) -> log.info("Received new message in example-topic: {}", input))
-                .mapValues(v -> new AnotherPayloadDto("foo", v))
-                .peek((k, v) -> log.info("Publishing output: {}", v));
+    public Function<ExamplePayloadDto, AnotherPayloadDto> process() {
+        return input -> {
+            log.info("Received new message in example-topic: {}", input);
+            AnotherPayloadDto output = new AnotherPayloadDto("foo", input);
+            log.info("Publishing output: {}", output);
+            return output;
+        };
     }
 
     @Bean
