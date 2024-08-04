f7e13ba166f4fa8fe4774680e865adaa374d07c7
Timon Back <timonback@users.noreply.github.com>
2024-08-04 17:22:04 +0200

    feat(core): remove studio work-around for yaml
    Since js `@asyncapi/parser@3.2.1`, the workaround for async api studio for date strings to need quotes is no longer needed

--- springwolf-core/src/main/java/io/github/springwolf/core/asyncapi/components/examples/walkers/yaml/DefaultExampleYamlValueSerializer.java
+++ springwolf-core/src/main/java/io/github/springwolf/core/asyncapi/components/examples/walkers/yaml/DefaultExampleYamlValueSerializer.java
@@ -4,24 +4,11 @@ package io.github.springwolf.core.asyncapi.components.examples.walkers.yaml;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
-import io.github.springwolf.core.configuration.properties.SpringwolfConfigProperties;
 import io.swagger.v3.core.util.Yaml;
 
 public class DefaultExampleYamlValueSerializer implements ExampleYamlValueSerializer {
 
-    private final ObjectMapper yamlMapper;
-
-    public DefaultExampleYamlValueSerializer(SpringwolfConfigProperties springwolfConfigProperties) {
-        this.yamlMapper = Yaml.mapper();
-
-        if (springwolfConfigProperties.isStudioCompatibility()) {
-            // AsyncApi Studio has problems with missing quotes on dates, so we add quotes on every example
-            // see https://github.com/springwolf/springwolf-core/issues/820
-            ((YAMLFactory) yamlMapper.getFactory()).disable(YAMLGenerator.Feature.MINIMIZE_QUOTES);
-        }
-    }
+    private final ObjectMapper yamlMapper = Yaml.mapper();
 
     @Override
     public String writeDocumentAsYamlString(JsonNode node) throws JsonProcessingException {
