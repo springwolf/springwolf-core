1ddde9c4ea107797547ddd0e84a10d04bdf3cb1d
Norbert Wirges <norbert.wirges@koerber.com>
2024-11-13 18:30:56 +0100

    fix(binding): added @ConditionalOnProperty to generic binding autoconfiguration to avoid errors due to missing bean (#1075)

--- springwolf-add-ons/springwolf-generic-binding/src/main/java/io/github/springwolf/addons/generic_binding/annotation/configuration/SpringwolfGenericBindingAutoConfiguration.java
+++ springwolf-add-ons/springwolf-generic-binding/src/main/java/io/github/springwolf/addons/generic_binding/annotation/configuration/SpringwolfGenericBindingAutoConfiguration.java
@@ -3,12 +3,15 @@ package io.github.springwolf.addons.generic_binding.annotation.configuration;
 
 import io.github.springwolf.addons.generic_binding.annotation.processor.AsyncGenericOperationBindingProcessor;
 import io.github.springwolf.core.asyncapi.scanners.bindings.BindingProcessorPriority;
+import io.github.springwolf.core.configuration.properties.SpringwolfConfigConstants;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.util.StringValueResolver;
 
 @Configuration
+@ConditionalOnProperty(name = SpringwolfConfigConstants.SPRINGWOLF_ENABLED, havingValue = "true", matchIfMissing = true)
 public class SpringwolfGenericBindingAutoConfiguration {
 
     @Bean
