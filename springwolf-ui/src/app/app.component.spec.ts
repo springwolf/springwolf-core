3b896c74d87679b4dfb759d6e3230c8fd2991040
David Müller <david.mueller@codecentric.de>
2024-12-13 15:37:13 +0100

    chore(springwolf-ui): re-add smoke-test for complete app
    Co-authored-by: Timon Back <timonback@users.noreply.github.com>

---
+++ springwolf-ui/src/app/app.component.spec.ts
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+import { AppComponent } from "./app.component";
+import { render, screen, waitFor, within } from "@testing-library/angular";
+import { appConfig } from "./app.config";
+import { MatIconTestingModule } from "@angular/material/icon/testing";
+
+describe("AppComponent", () => {
+  test("should render", async () => {
+    await render(AppComponent, {
+      providers: appConfig.providers,
+      imports: [MatIconTestingModule],
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getAllByText("Springwolf example project", { exact: false })
+          .length
+      ).toBeGreaterThan(0);
+    });
+
+    // get menu items
+    const articles = screen.getAllByRole("article");
+
+    // server menu
+    const servers = articles.find(
+      (article) => within(article).queryByText("Servers") !== null
+    );
+    expect(servers).not.toBeNull();
+    expect(within(servers!!).getAllByRole("article").length).toBeGreaterThan(0);
+
+    // channel menu
+    const channels = articles.find(
+      (article) => within(article).queryByText("Channels") !== null
+    );
+    expect(channels).not.toBeNull();
+    expect(within(channels!!).getAllByRole("article").length).toBeGreaterThan(
+      0
+    );
+
+    // schema menu
+    const schemas = articles.find(
+      (article) => within(article).queryByText("Schemas") !== null
+    );
+    expect(schemas).not.toBeNull();
+    expect(within(schemas!!).getAllByRole("article").length).toBeGreaterThan(0);
+  });
+});
