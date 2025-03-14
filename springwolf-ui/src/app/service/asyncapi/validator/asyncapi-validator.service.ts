a964e5845b843a2a6de9b43ac570c5e00c235e80
David Müller <david.mueller@codecentric.de>
2025-03-14 16:10:15 +0100

    feat(springwolf-ui): support for strict and lenient validation
    Co-authored-by: Timon Back <timonback@users.noreply.github.com>

--- springwolf-ui/src/app/service/asyncapi/validator/asyncapi-validator.service.ts
+++ springwolf-ui/src/app/service/asyncapi/validator/asyncapi-validator.service.ts
@@ -1,31 +1,33 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-import { Injectable } from "@angular/core";
+import {Injectable} from "@angular/core";
 import Ajv from "ajv";
 import expectedSchema from "./server-async-api.schema.json";
-import { ServerAsyncApi } from "../models/asyncapi.model";
+import {ServerAsyncApi} from "../models/asyncapi.model";
 
 @Injectable()
 export class AsyncApiValidatorService {
-  // private readonly ajv = new Ajv({allErrors: true, removeAdditional: true});
-  private readonly ajvInStrictMode = new Ajv({
-    allErrors: true,
-    removeAdditional: false,
-  });
+    private readonly ajv = new Ajv({allErrors: true});
 
-  public validate(item: ServerAsyncApi) {
-    // const validate = this.ajv.compile(expectedSchema);
-    // const isValid = validate(item)
-    // if(!isValid) {
-    // console.warn("Validation error while parsing asyncapi file in Springwolf format", validate.errors)
-    // }
+    public validate(item: ServerAsyncApi) {
+        // configure strict mode
+        this.ajv.opts.removeAdditional = false;
+        const validateStrict = this.ajv.compile(expectedSchema)
+        const isValidInStrictMode = validateStrict(item)
+        if (!isValidInStrictMode) {
+            console.warn("Validation error while parsing asyncapi file in Springwolf format (strict mode)", validateStrict.errors)
+        }
+        this.ajv.removeSchema(expectedSchema);
 
-    const validateStrict = this.ajvInStrictMode.compile(expectedSchema);
-    const isValidInStrictMode = validateStrict(item);
-    if (!isValidInStrictMode) {
-      console.info(
-        "Validation error while parsing asyncapi file in Springwolf format (strict mode)",
-        validateStrict.errors
-      );
+        // configure lenient mode
+        this.ajv.opts.removeAdditional = true;
+
+        const validateLenient = this.ajv.compile(expectedSchema)
+        const isValidInLenientMode = validateLenient(item);
+        if (!isValidInLenientMode) {
+            console.warn(
+                "Validation error while parsing asyncapi file in Springwolf format (lenient mode)",
+                validateLenient.errors
+            );
+        }
     }
-  }
 }
