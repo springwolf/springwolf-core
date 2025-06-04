db2d7f94193ee6999f18aae1286e57b9658f9cf3
antoine-vaisset <antoine.vaisset@ext-energy-pool.eu>
2025-06-04 09:17:54 +0200

    feat(sqs): add SNS headers when sending message from springwolf ui
    - standardizes plugin producer/controller header management

--- springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/springwolf/plugins/sqs/producer/SpringwolfSqsProducer.java
+++ springwolf-plugins/springwolf-sqs-plugin/src/main/java/io/github/springwolf/plugins/sqs/producer/SpringwolfSqsProducer.java
@@ -2,10 +2,14 @@
 package io.github.springwolf.plugins.sqs.producer;
 
 import io.awspring.cloud.sqs.operations.SqsTemplate;
+import io.github.springwolf.core.controller.dtos.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -21,11 +25,24 @@ public class SpringwolfSqsProducer {
         return template.isPresent();
     }
 
-    public void send(String channelName, Message<?> message) {
+    public void send(String channelName, Map<String, MessageDto.HeaderValue> headers, Object payload) {
         if (template.isPresent()) {
+            Message<?> message = MessageBuilder.withPayload(payload)
+                    .copyHeaders(mapHeaders(headers))
+                    .build();
             template.get().send(channelName, message);
         } else {
             log.warn("SQS producer is not configured");
         }
+    }
+
+    private static Map<String, String> mapHeaders(Map<String, MessageDto.HeaderValue> headers) {
+        return headers.entrySet().stream()
+                .collect(
+                        HashMap::new,
+                        (m, e) -> m.put(
+                                e.getKey(),
+                                e.getValue() == null ? null : e.getValue().stringValue()),
+                        HashMap::putAll);
     }
 }
