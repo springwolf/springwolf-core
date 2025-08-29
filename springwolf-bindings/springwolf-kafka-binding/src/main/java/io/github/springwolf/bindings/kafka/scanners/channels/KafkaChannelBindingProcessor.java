a68dae35b73378f2e31258ecfff7dd8df90488bb
sam0r040 <93372330+sam0r040@users.noreply.github.com>
2025-08-29 16:20:01 +0200

    Update KafkaChannelBindingProcessor.java

--- springwolf-bindings/springwolf-kafka-binding/src/main/java/io/github/springwolf/bindings/kafka/scanners/channels/KafkaChannelBindingProcessor.java
+++ springwolf-bindings/springwolf-kafka-binding/src/main/java/io/github/springwolf/bindings/kafka/scanners/channels/KafkaChannelBindingProcessor.java
@@ -14,6 +14,9 @@ import java.util.Arrays;
 
 public class KafkaChannelBindingProcessor extends AbstractChannelBindingProcessor<KafkaAsyncChannelBinding> {
 
+    private static final KafkaChannelTopicConfiguration EMPTY_TOPIC_CONFIGURATION =
+            KafkaChannelTopicConfiguration.builder().build();
+
     public KafkaChannelBindingProcessor(StringValueResolver stringValueResolver) {
         super(stringValueResolver);
     }
