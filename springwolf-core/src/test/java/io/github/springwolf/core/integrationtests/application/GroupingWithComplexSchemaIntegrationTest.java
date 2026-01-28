136e6fc44e833d2041c9b5cc5e35857ae4364e64
Timon Back <timonback@users.noreply.github.com>
2026-01-28 21:25:16 +0100

    test(core): minor cleanup and adding reference to verifying collections

--- springwolf-core/src/test/java/io/github/springwolf/core/integrationtests/application/GroupingWithComplexSchemaIntegrationTest.java
+++ springwolf-core/src/test/java/io/github/springwolf/core/integrationtests/application/GroupingWithComplexSchemaIntegrationTest.java
@@ -3,10 +3,11 @@ package io.github.springwolf.core.integrationtests.application;
 
 import io.github.springwolf.asyncapi.v3.model.AsyncAPI;
 import io.github.springwolf.asyncapi.v3.model.channel.message.MessageObject;
+import io.github.springwolf.asyncapi.v3.model.components.ComponentSchema;
 import io.github.springwolf.asyncapi.v3.model.schema.SchemaReference;
 import io.github.springwolf.core.asyncapi.AsyncApiService;
 import io.github.springwolf.core.fixtures.SpringwolfIntegrationTest;
-import io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication;
+import io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,7 @@ import org.springframework.test.context.TestPropertySource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Nested
-@SpringBootTest(classes = PublisherGroupingComplexSchemaApplication.class)
+@SpringBootTest(classes = ComplexSchemaApplication.class)
 @SpringwolfIntegrationTest
 @TestPropertySource(
         properties = {
@@ -34,36 +35,48 @@ class GroupingWithComplexSchemaIntegrationTest {
     private AsyncApiService asyncApiService;
 
     @Test
-    void shouldFindOnlyForGroupFoo() {
+    void shouldFindOnlyForGroupUserMessage() {
         AsyncAPI asyncAPI = asyncApiService.getForGroupName("UserMessage").get();
 
-        assertThat(asyncAPI.getChannels().keySet()).containsExactlyInAnyOrder("publisher-channel");
-        assertThat(asyncAPI.getChannels().get("publisher-channel").getMessages())
+        assertThat(asyncAPI.getChannels().keySet()).containsExactlyInAnyOrder("publisher-channel2");
+        assertThat(asyncAPI.getChannels().get("publisher-channel2").getMessages())
                 .containsOnlyKeys(
-                        "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.User");
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User");
         assertThat(asyncAPI.getOperations())
-                .containsOnlyKeys("publisher-channel_send_publish1", "publisher-channel_send_publish2");
+                .containsOnlyKeys("publisher-channel2_send_publish1", "publisher-channel2_send_publish2");
         assertThat(asyncAPI.getComponents().getMessages())
                 .containsOnlyKeys(
-                        "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.User");
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User");
         assertThat(asyncAPI.getComponents().getSchemas())
                 .containsOnlyKeys(
                         "HeadersNotDocumented",
-                        "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.User",
-                        "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.Address",
-                        "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.City");
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User",
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.Address",
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.City");
 
         MessageObject userMessage = (MessageObject)
                 asyncAPI.getComponents()
                         .getMessages()
                         .get(
-                                "io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.User");
+                                "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User");
         assertThat(userMessage.getPayload().getMultiFormatSchema().getSchema()).isInstanceOf(SchemaReference.class);
-        SchemaReference fooSchemaRef = (SchemaReference)
+        SchemaReference userMessageToSchemaRef = (SchemaReference)
                 userMessage.getPayload().getMultiFormatSchema().getSchema();
-        assertThat(fooSchemaRef.getRef())
+        assertThat(userMessageToSchemaRef.getRef())
                 .isEqualTo(
-                        "#/components/schemas/io.github.springwolf.core.integrationtests.application.complexSchema.PublisherGroupingComplexSchemaApplication.ComplexPublisher.User");
+                        "#/components/schemas/io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User");
+
+        // Verify that collection property is correctly represented using `items`
+        ComponentSchema addressesSchema = (ComponentSchema) asyncAPI.getComponents()
+                .getSchemas()
+                .get(
+                        "io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.User")
+                .getSchema()
+                .getProperties()
+                .get("addresses");
+        assertThat(addressesSchema.getSchema().getItems().getReference().getRef())
+                .isEqualTo(
+                        "#/components/schemas/io.github.springwolf.core.integrationtests.application.complexSchema.ComplexSchemaApplication.ComplexPublisher.Address");
     }
 
     @Test
